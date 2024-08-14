4e59225025a267a0ad9f93244bfff01c3c1a7b90
Mr._Aman <157211536+Aman0740@users.noreply.github.com>
2024-08-14 19:01:41 +0530

    update

---
+++ add_to_cart/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function CartItem() {
+  const [count, setCount] = useState(0);
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleAddToCart = () => {
+    setCount(1);
+    setShowNotification(true);
+    setTimeout(() => setShowNotification(false), 2000); // hide notification after 3 seconds
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  return (
+    <div className='App-header'>
+      <img src="https://5.imimg.com/data5/SELLER/Default/2023/1/MC/SP/FN/69519039/51vz6i9-cml-sl1500-.jpg" height={200} width={200} />
+      <h2>Fanta 50$</h2>
+
+      {count === 0 ? (
+        <button onClick={handleAddToCart}>Add to Cart</button>
+      ) : (
+        <div>
+          <button onClick={decrement}>-</button>
+          &nbsp;
+          <span>{count}</span>
+          &nbsp;
+          <button onClick={increment}>+</button>
+        </div>
+      )}
+      {showNotification && <p>Product added to your cart!</p>}
+    </div>
+  );
+}
+
+export default CartItem;
